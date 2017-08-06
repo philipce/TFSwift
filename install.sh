cb28ad8cdb01c9af47c4e665ffe029c2eebea750
Philip Erickson <philiperickson@gmail.com>
2017-08-06 16:05:45 -0700

    Install script to install libtensorflow

---
+++ install.sh
@@ -0,0 +1,9 @@
+OSABR=$(echo $(uname)|tr '[:upper:]' '[:lower:]')
+DWN=/tmp/libtensorflow.tgz
+VERSION=1.2.1
+URL=https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-$OSABR-x86_64-$VERSION.tar.gz
+echo $URL
+wget $URL -O $DWN
+sudo tar xvf $DWN -C /usr/local ./lib/libtensorflow.so
+rm -f $DWN
+ls -l /usr/local/lib/libtensorflow.so

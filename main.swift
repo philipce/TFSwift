677e117104cc7e14dc91e8d207c57ede5124d248
Philip Erickson <philiperickson@gmail.com>
2017-08-06 16:05:24 -0700

    Add matrix op and model load example

--- main.swift
+++ main.swift
@@ -38,20 +38,20 @@ try TF.Open()
 // try TF.Open("/path/to/DLL/of/libtensorflow.so")
 
 //--------------------------------------------------
-// FUNCTIONALITY
+// HELLO
 //--------------------------------------------------
 
 // define a string tensor
 let tensor = try TF.Tensor.Scalar("Hello, Perfect TensorFlow! 🇨🇳🇨🇦")
 
 // declare a new graph
-let g = try TF.Graph()
+var g = try TF.Graph()
 
 // turn the tensor into an operation
 let op = try g.const(tensor: tensor, name: "hello")
 
 // run a session
-let o = try g.runner().fetch(op).addTarget(op).run()
+var o = try g.runner().fetch(op).addTarget(op).run()
 
 // decode the result      
 let decoded = try TF.Decode(strings: o[0].data, count: 1)
@@ -59,3 +59,48 @@ let decoded = try TF.Decode(strings: o[0].data, count: 1)
 // check the result
 let s2 = decoded[0].string
 print(s2)
+
+//--------------------------------------------------
+// MATRIX OPERATIONS
+//--------------------------------------------------
+
+/* Matrix Multiply:
+| 1 2 |   |0 1|   |0 1|
+| 3 4 | * |0 0| = |0 3|
+*/
+// input the matrix.
+let tA = try TF.Tensor.Matrix([[1, 2], [3, 4]])
+let tB = try TF.Tensor.Matrix([[0, 0], [1, 0]])
+
+// adding tensors to graph 
+g = try TF.Graph()
+let A = try g.const(tensor: tA, name: "Const_0")
+let B = try g.const(tensor: tB, name: "Const_1")
+
+// define matrix multiply operation
+let v = try g.matMul(l: A, r: B, name: "v", transposeB: true)
+
+// run the session
+o = try g.runner().fetch(v).addTarget(v).run()
+let m:[Float] = try o[0].asArray()
+print(m)
+// m shall be [0, 1, 0, 3]
+
+//--------------------------------------------------
+// MODEL LOAD
+//--------------------------------------------------
+
+g = try TF.Graph()
+
+// the meta signature info defined in a saved model
+let metaBuf = try TF.Buffer()
+
+// load the session
+let session = try g.load(
+	exportDir: "/path/to/saved/model",
+	tags: ["tag1", "tag2"],
+	metaGraphDef: metaBuf)
+
+
+
+
